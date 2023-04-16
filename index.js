c34144e2267883b4463683d83807cc5d7ca38c9d
Samuel Gomez <contact@samuelgomez.fr>
2023-04-16 11:17:04 +0200

    add db

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 const express = require('express');
+const { connexion } = require('./db');
 
-const { PORT = '80' } = process.env;
+const { PORT = '3001' } = process.env;
 const ASSETS_PATH = '/assets';
 
-const app = express();
+connexion.then(async db => {
+  const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
-app.use(ASSETS_PATH, express.static('assets'));
+  app.set('view engine', 'pug');
+  app.set('views', __dirname + '/views');
+  app.use(ASSETS_PATH, express.static('assets'));
 
-app.use((req, res, next) => {
-  res.locals.assetsPath = ASSETS_PATH;
-  next();
+  app.use((req, res, next) => {
+    res.locals.assetsPath = ASSETS_PATH;
+    next();
+  });
+
+  app.get('/', (req, res) => {
+    res.render('pages/index', { title: 'Accueil' });
+  });
+
+  app.get('/search', function (req, res) {
+    res.render('pages/search', { title: 'Recherche' });
+  });
+
+  app.get('/cart', function (req, res) {
+    res.render('pages/cart', { title: 'Panier' });
+  });
+
+  app.get('/contact', function (req, res) {
+    res.render('pages/contact', { title: 'Contact' });
+  });
+
+  app.get('/connexion', function (req, res) {
+    res.render('pages/connexion', { title: 'Connexion' });
+  });
+
+  app.get('/inscription', function (req, res) {
+    res.render('pages/inscription', { title: 'Inscription' });
+  });
+
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 });
-
-app.get('/', (req, res) => {
-  res.render('pages/index', { title: 'Accueil' });
-});
-
-app.get('/search', function (req, res) {
-  res.render('pages/search', { title: 'Recherche' });
-});
-
-app.get('/cart', function (req, res) {
-  res.render('pages/cart', { title: 'Panier' });
-});
-
-app.get('/contact', function (req, res) {
-  res.render('pages/contact', { title: 'Contact' });
-});
-
-app.get('/connexion', function (req, res) {
-  res.render('pages/connexion', { title: 'Connexion' });
-});
-
-app.get('/inscription', function (req, res) {
-  res.render('pages/inscription', { title: 'Inscription' });
-});
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
